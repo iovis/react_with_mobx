a8b5a5c8579b992b59a99c582fab279656492601
David Marchante <davidmarchan@gmail.com>
2018-02-24 12:19:31 +0100

    Added width and height

--- src/models/card.js
+++ src/models/card.js
@@ -1,10 +1,15 @@
 import { observable, action } from 'mobx';
 
+const MINIMUM_WIDTH = 80;
+const MINIMUM_HEIGHT = 50;
+
 export default class Card {
   id = Date.now();
   @observable body;
   @observable x = 0;
   @observable y = 0;
+  @observable width = 100;
+  @observable height = 50;
 
   constructor(body) {
     this.body = body;
@@ -13,12 +18,38 @@ export default class Card {
   @action
   moveX(movedX) {
     this.x += movedX;
-    if (this.x < 0) this.x = 0;
+
+    if (this.x < 0) {
+      return this.x = 0;
+    }
+
+    // Don't let it go out of bounds
+    if (this.x > window.innerWidth - this.width) {
+      this.x = window.innerWidth - this.width;
+    }
   }
 
   @action
   moveY(movedY) {
     this.y += movedY;
-    if (this.y < 0) this.y = 0;
+
+    if (this.y < 0) {
+      return this.y = 0;
+    }
+
+    // Don't let it go out of bounds
+    if (this.y > window.innerHeight - this.height) {
+      this.y = window.innerHeight - this.height;
+    }
+  }
+
+  @action
+  setWidth(width) {
+    this.width = (width < MINIMUM_WIDTH) ? MINIMUM_WIDTH : width;
+  }
+
+  @action
+  setHeight(height) {
+    this.height = (height < MINIMUM_HEIGHT) ? MINIMUM_HEIGHT : height;
   }
 }
--- src/models/card.spec.js
+++ src/models/card.spec.js
@@ -1,6 +1,12 @@
 import Card from './card';
 
+let card;
+
 describe('Card', () => {
+  beforeEach(() => {
+    card = new Card('heyo!');
+  });
+
   it('generates an id', () => {
     // Mock time
     const now = Date.now();
@@ -11,14 +17,67 @@ describe('Card', () => {
     expect(card.id).toBeCloseTo(now);
   });
 
-  it('generates the proper body',() => {
-    const card = new Card('heyo!');
+  it('generates the proper body', () => {
     expect(card.body).toEqual('heyo!');
   });
 
-  it('generates the coordinates',() => {
-    const card = new Card('heyo!');
-    expect(card.x).toEqual(0);
-    expect(card.y).toEqual(0);
+  describe('coordinates', () => {
+    it('generates the coordinates', () => {
+      expect(card.x).toEqual(0);
+      expect(card.y).toEqual(0);
+    });
+
+    it('properly moves the card', () => {
+      card.moveX(23);
+      card.moveY(32);
+
+      expect(card.x).toEqual(23);
+      expect(card.y).toEqual(32);
+    });
+
+    it('does not create coordinates lower than 0', () => {
+      card.moveX(-23);
+      card.moveY(-32);
+
+      expect(card.x).toEqual(0);
+      expect(card.y).toEqual(0);
+    });
+
+    it('does not create coordinates greater than the window', () => {
+      const maxWidth  = 1280;
+      const maxHeight = 480;
+
+      window.innerWidth = maxWidth;
+      window.innerHeight = maxHeight;
+
+      card.moveX(maxWidth + card.width + 24);
+      card.moveY(maxHeight + card.height + 52);
+
+      expect(card.x).toEqual(maxWidth - card.width);
+      expect(card.y).toEqual(maxHeight - card.height);
+    });
+  });
+
+  describe('shape', () => {
+    it('creates the card with default dimensions', () => {
+      expect(card.width).toEqual(100);
+      expect(card.height).toEqual(50);
+    });
+
+    it('lets you modify its dimensions', () => {
+      card.setWidth(500);
+      card.setHeight(300);
+
+      expect(card.width).toEqual(500);
+      expect(card.height).toEqual(300);
+    });
+
+    it('has a minimum width and height', () => {
+      card.setWidth(0);
+      card.setHeight(0);
+
+      expect(card.width).toBeGreaterThan(0);
+      expect(card.height).toBeGreaterThan(0);
+    });
   });
 });
