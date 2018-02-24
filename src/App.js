4a2db071616adb88750ff40fdcc2dc8b33b71eee
David Marchante <davidmarchan@gmail.com>
2018-02-24 15:58:54 +0100

    App composition

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Board from './components/Board';
 import CardList from './components/CardList';
+import Header from './components/Header';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <CardList store={this.props.store}/>
+      <div className="app">
+        <Header />
+        <CardList store={this.props.store}/>
+        <Board />
+      </div>
     );
   }
 }
